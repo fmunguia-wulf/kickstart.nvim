4c9c06e39fbc920a09002f4af29278eea7d73642
Patxi <fmunguiawulftange@ucsd.edu>
2025-02-12 00:50:54 -0800

    Starting fresh ...

--- init.lua
+++ init.lua
@@ -92,11 +92,19 @@ do
   -- Enable faster startup by caching compiled Lua modules
   vim.loader.enable()
 
+<<<<<<< HEAD
   -- Set <space> as the leader key
   -- See `:help mapleader`
   --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
   vim.g.mapleader = ' '
   vim.g.maplocalleader = ' '
+||||||| parent of 1acb9ae (added nerd font)
+-- Set to true if you have a Nerd Font installed and selected in the terminal
+vim.g.have_nerd_font = false
+=======
+-- Set to true if you have a Nerd Font installed and selected in the terminal
+vim.g.have_nerd_font = true
+>>>>>>> 1acb9ae (added nerd font)
 
   -- Set to true if you have a Nerd Font installed and selected in the terminal
   vim.g.have_nerd_font = false
