f46ca26579a9a8c8e280d034e0f91009b8ec0c0d
Francisco Munguia-Wulftange <fmunguiawulftange@ucsd.edu>
2026-06-11 10:31:38 +0200

    fix: sync custom plugins init.lua with upstream vim.pack version

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,29 +2,12 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
----@module 'lazy'
----@type LazySpec
-return {
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    opts = {
-      window = {
-        position = "left",
-        width = 30,
-      },
-    },
-    keys = {
-      { "<leader>e", ":Neotree toggle<CR>", desc = "Toggle file tree" },
-    },
-  },
 
-  {
-    "akinsho/toggleterm.nvim",
-    config = function()
-      require("toggleterm").setup({
-        size = 15,
-        open_mapping = [[<C-\>]],
-      })
-    end,
-  },
-}
+-- Iterate over all Lua files in the plugins directory and load them
+local plugins_dir = vim.fs.joinpath(vim.fn.stdpath 'config', 'lua', 'custom', 'plugins')
+for file_name, type in vim.fs.dir(plugins_dir) do
+  if type == 'file' and file_name:match '%.lua$' and file_name ~= 'init.lua' then
+    local module = file_name:gsub('%.lua$', '')
+    require('custom.plugins.' .. module)
+  end
+end
